59e1729eb3ed419c3eaa9daeaf3fc2f11c25670f
Aryan Mehta <aryannmehta@gmail.com>
2023-10-17 17:06:09 +0530

    ✨ Feat: Design and finish the Spotify Header

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,5 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
