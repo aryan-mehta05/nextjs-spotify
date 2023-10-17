f498b823ac246c46db73d9afe97dd34ba2c06224
Aryan Mehta <aryannmehta@gmail.com>
2023-10-17 16:28:04 +0530

    ✨ Feat: Finish creating MediaItem component

--- components/Library.tsx
+++ components/Library.tsx
@@ -4,6 +4,13 @@ import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
 
 import { Song } from "@/types";
+import { useUser } from "@/hooks/useUser";
+import useOnPlay from "@/hooks/useOnPlay";
+import useAuthModal from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
+import useSubscribeModal from "@/hooks/useSubscribeModal";
+
+import MediaItem from "@/components/MediaItem";
 
 interface LibraryProps {
   songs: Song[];
