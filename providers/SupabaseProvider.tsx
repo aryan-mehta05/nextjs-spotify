64416ffa37c59294fe34d7b2b9c8271b06c8df44
Aryan Mehta <aryannmehta@gmail.com>
2023-10-18 12:09:47 +0530

    ⚙️  Create the supabase provider

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useState } from "react";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+import { Database } from "@/types_db";
+
+interface SupabaseProviderProps {
+  children: React.ReactNode;
+};
+
+const SupabaseProvider = ({
+  children
+}: SupabaseProviderProps) => {
+  const [supabaseClient] = useState(() => 
+    createClientComponentClient<Database>()
+  );
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
+  );
+};
+
+export default SupabaseProvider;
