7a37c8bfbb8cd280e37e7ec25a5dbcbd42598197
Aryan Mehta <aryannmehta@gmail.com>
2023-10-19 15:42:57 +0530

    💄 CSS: Fix Song Item css (trancate --> truncate)

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -31,7 +31,7 @@ const SongItem = ({
         />
       </div>
       <div className="flex flex-col items-start w-full pt-4 gap-y-1">
-        <p className="font-semibold trancate w-full">
+        <p className="font-semibold truncate w-full">
           {data.title}
         </p>
         <p className="text-neutral-400 text-sm pb-4 w-full truncate">
